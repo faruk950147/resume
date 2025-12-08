be835bcc0d72d78973cea9029c82ff1969a37132
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-08 21:31:58 +0600

    I have added php

---
+++ index.php
@@ -0,0 +1,81 @@
+
+<?php
+    include_once "include/open_html.php";
+?>
+
+<?php
+    include_once "include/header.php";
+?>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <?php
+    include_once "include/hero_section.php";
+    ?>
+    <!-- /Hero Section -->
+
+    <!-- About Section -->
+    <?php
+    include_once "include/about_me.php";
+    ?>   
+    <!-- /About Section -->
+
+    <!-- Resume Section -->
+    <?php
+    include_once "include/resume.php";
+    ?>   
+    <!-- /Resume Section -->
+
+    <!-- Services Section -->
+    <?php
+    include_once "include/services_section.php";
+    ?> 
+    <!-- /Services Section -->
+
+    <!-- Stats Section -->
+    <?php
+    include_once "include/stats_section.php";
+    ?> 
+    <!-- /Stats Section -->
+
+    <!-- Portfolio Section -->
+    <?php
+    include_once "include/portfolio_section.php";
+    ?> 
+    <!-- /Portfolio Section -->
+
+    <!-- Pricing Section -->
+    <?php
+    include_once "include/pricing_section.php";
+    ?> 
+    <!-- /Pricing Section -->
+
+    <!-- Faq Section -->
+    <?php
+    include_once "include/faq_section.php";
+    ?> 
+    <!-- /Faq Section -->
+
+    <!-- Testimonials Section -->
+    <?php
+    include_once "include/testimonials.php";
+    ?> 
+    <!-- /Testimonials Section -->
+
+    <!-- Contact Section -->
+    <?php
+    include_once "include/contact.php";
+    ?> 
+    <!-- /Contact Section -->
+
+  </main>
+
+<?php
+    include_once "include/footer.php";
+?>
+ 
+
+<?php
+    include_once "include/closed_html.php";
+?>
